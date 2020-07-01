d7e685f7fd46a60ca1928725ddb84587c0ae7423
Cassiano Aquino <cassianoaquino@me.com>
2020-07-01 15:30:35 +0100

    fix k9s version

--- packages/k9s/dynamic/recipe.rb
+++ packages/k9s/dynamic/recipe.rb
@@ -7,7 +7,7 @@ class K9s < FPM::Cookery::Recipe
   require 'open-uri'
   require 'json'
 
-  version     JSON.parse(URI.parse("https://api.github.com/repos/derailed/k9s/releases").read).first['tag_name']
+  version     JSON.parse(URI.parse("https://api.github.com/repos/derailed/k9s/releases").read).first['tag_name'][1..-1]
   revision    0
 
   source "https://github.com/derailed/k9s/releases/download/#{version}/k9s_Linux_x86_64.tar.gz"
