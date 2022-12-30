cb5ba5458c4e0543e97e5cdb47bf197244d83e33
Cassiano Aquino <caquino@users.noreply.github.com>
2022-12-30 11:22:28 +0000

    Update build.sh
    add redis-bash to repository

--- build.sh
+++ build.sh
@@ -9,3 +9,9 @@ do
 done
 mkdir repository
 mv packages/*/*/pkg/*.deb repository/
+curl -s https://api.github.com/repos/caquino/redis-bash/releases/latest \
+| grep "browser_download_url.*deb" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+mv *.deb repository/
